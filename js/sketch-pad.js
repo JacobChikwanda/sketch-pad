774ec0948098865c746c41a0cb831fd68f0d148d
Jacob Chikwanda <jacofaithcob@gmail.com>
2024-10-21 15:22:10 +0200

    Added functionality to determine the position of the mouse | added path logic

--- js/sketch-pad.js
+++ js/sketch-pad.js
@@ -1,8 +1,12 @@
 class SketchPad {
-    constructor(sketchpad_element_ref, size=500) {
+    constructor(sketchpad_element_ref, size = 500) {
+
+        // Attributes
+        this.is_drawing = false;
+
         // Create canvas in javascript
         this.canvas = document.createElement('canvas');
-        
+
         // How add styling to the canvas
         this.canvas.width = size;
         this.canvas.height = size;
@@ -13,5 +17,41 @@ class SketchPad {
 
         // How to add element to sketchpad
         sketchpad_element_ref.appendChild(this.canvas)
+
+        // Handling all canvas events
+        this.#handleEvents()
+    }
+
+    // Function to listen to events
+    #handleEvents() {
+        // Handles what happens when mouse is clicked
+        this.canvas.addEventListener('mousedown', (e) => {
+            this.is_drawing = true;
+            const mouse_position = this.#getMousePosition(e);
+            console.log(mouse_position)
+        });
+
+        // Handles what happens when mouse is moving
+        this.canvas.addEventListener('mousemove', (e) => {
+            if (this.is_drawing) {
+                const mouse_position = this.#getMousePosition(e);
+                console.log(mouse_position)
+            }
+        });
+
+        // Handles what happens when mouse is no longer clicked
+        this.canvas.addEventListener('mouseup', (e) => {
+            this.is_drawing = false;
+        })
+    }
+
+    // Function gets the current position of the mouse
+    #getMousePosition(e) {
+        const rect = this.canvas.getBoundingClientRect();
+        const mouse_position = [
+            Math.round(e.clientX - rect.left),
+            Math.round(e.clientY - rect.top)
+        ];
+        return mouse_position;
     }
 }
